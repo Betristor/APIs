0fa1ad1982f0f6f173477ed9549a74e02dcf1478
Yuheng Zhang <55777837+Betristor@users.noreply.github.com>
2024-04-03 15:37:02 +0800

    Add unit parameter to getDistance function

--- BaiduMapAPI/getDistance.py
+++ BaiduMapAPI/getDistance.py
@@ -5,15 +5,21 @@ from BaiduMapAPI.getAK import getAK
 from BaiduMapAPI.getPosition import getPosition
 
 
-def getDistance(start, end):
+def getDistance(start, end, unit="km"):
     url = "https://api.map.baidu.com/directionlite/v1/driving?origin={}&destination={}&ak={}".format(
         start, end, getAK()["AK"]
     )
     res = requests.get(url)
     json_data = json.loads(res.text)
 
-    if json_data["status"] == 0:
-        return json_data["result"]["routes"][0]["distance"]
+    if json_data["status"] == 200:
+        if unit == "km":
+            return json_data["result"]["routes"][0]["distance"] / 1000
+        elif unit == "mi":
+            return json_data["result"]["routes"][0]["distance"] / 1609
+        else:
+            print("Invalid unit")
+            return -1
     else:
         print(json_data["message"])
         return -1
